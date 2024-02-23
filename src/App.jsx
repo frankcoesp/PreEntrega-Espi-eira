6d6949556cc9ca5329219863a8c0b19ec293f2c1
Franco Espiñeira <espifranjor@hotmail.com>
2024-02-23 16:02:28 -0300

    estilo de fondo listo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './componentes/Navbar'
+
+function App() {
+    return (
+        <div className='container'>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default App
--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Nabvar/Navbar.css'
 
 function Navbar() {
     return (
-        <div>
-            <h1>hola mundo </h1>
+        <div className='navbar'>
+            <img src="" alt="" className='logo'/>
+            <ul>
+                <li className=''>Home</li>
+                <li className=''>Contact</li>
+                <li className=''>About us</li>
+                <li className=''>Products</li>
+            </ul>
+
+        <div className='search-box'>
+        <input type="text" placeholder='Search'/>
+        <img src="" alt="" />
+        </div>
+
         </div>
     )
 }
