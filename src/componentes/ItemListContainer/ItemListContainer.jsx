b89cb5b403d33236db30aff576e5c64d7ec52e7c
Franco Espiñeira <espifranjor@hotmail.com>
2024-02-26 14:32:18 -0300

    PreEntrega finalizada

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../../componentes/ItemListContainer/ItemListContainer.css'
 
 function ItemListContainer(props) {
     return (
         <div>
-            <h1>{props.greeting}</h1>
+            <h1 className='title'>{props.greeting}</h1>
         </div>
     )
 }
